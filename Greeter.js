ef741c7b2f91ed6cc2dac0b38333db1cd1353a0b
wiame kazdari <kazdari.wiam@gmail.com>
2019-08-25 14:28:37 +0100

    set up properties

--- Greeter.js
+++ Greeter.js
@@ -4,9 +4,30 @@
     return new Greeter.init(firstname, lastname, language);
   }
 
+  //{ these variables
+  var supportedLangs = ['es', 'en'];
+
+  var greetings = {
+    en : 'Hello',
+    es : 'Holla',
+  };
+
+  var formalGreetings = {
+    en : 'Greetings',
+    es : 'Saludos',
+  };
+
+  var logMessages = {
+    en : 'Logged in' ,
+    es : 'Inicio sesion',
+  };
+
+  //} are not something exposed to the outside world
+
   Greeter.prototype = {};
 
   Greeter.init = function(firstname, lastname, language){
+    //any object built here will have access to any method and property built in Greeter.prototype
     var self = this;
     self.firstname = firstname || ' ';
     self.lastname = lastname || ' ';
